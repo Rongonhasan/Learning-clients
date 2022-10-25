9d8fa74f70f59f3c34d5620e021c712c8ce260eb
rongon <rongonhasan2@gmail.com>
2022-10-24 23:49:37 -0700

    header part

---
+++ src/Navbar/FAQ.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FAQ = () => {
+    return (
+        <div>
+            <h1>faq pepole</h1>
+        </div>
+    );
+};
+
+export default FAQ;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,31 @@
+
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Navbar/Blog";
+import Courses from "../Navbar/Courses";
+import FAQ from "../Navbar/FAQ";
 
 export const routes = createBrowserRouter([
     {
-        path: "about",
+        path: "/",
         element: <Main></Main>,
         children : [
-            
+            {
+                path: '/',
+                element: <header></header>,
+              },
+            {
+                path: '/courses',
+                element: <Courses></Courses>,
+              },
+              {
+                path: '/FAQ',
+                element: <FAQ></FAQ>,
+              },
+              {
+                path: '/Blog',
+                element: <Blog></Blog>,
+              }
         ]
       },
 ])
